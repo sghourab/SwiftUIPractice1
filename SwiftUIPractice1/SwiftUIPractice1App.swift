f6134cdc1e5ffb4653c461fa8ae0b3e5cb566964
Samar Ghourab <samarghourab@gmail.com>
2022-09-26 11:08:24 -0400

    Initial Commit

---
+++ SwiftUIPractice1/SwiftUIPractice1App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPractice1App.swift
+//  SwiftUIPractice1
+//
+//  Created by Summer Crow on 2022-09-26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPractice1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
